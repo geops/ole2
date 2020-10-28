62e1d078e5d063ed6e19b25328b9254df69729e5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-28 09:46:41 +0000

    Deploy geops/openlayers-editor to github.com/geops/openlayers-editor.git:gh-pages

---
+++ cypress/integration/ole.spec.js
@@ -0,0 +1,10 @@
+describe('OLE', function () {
+
+  beforeEach(function () {
+    cy.visit('/');
+  });
+
+  it('should initialize OLE toolbar', function () {
+    cy.get('#ole-toolbar').should('exist');
+  });
+});
