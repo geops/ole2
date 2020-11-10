3f81b304146553393c6700177ae50a3e41983820
lb1c <lb1c@users.noreply.github.com>
2020-11-10 08:03:05 +0000

    Deploying to gh-pages from  @ 4c7ffe9df69aea8bfb9a8503528e6f369470a738 🚀

--- src/interaction/index.js
+++
@@ -1,4 +0,0 @@
-import Select from './select';
-
-// eslint-disable-next-line import/prefer-default-export
-export { Select };
--- src/service/index.js
+++
@@ -1,7 +0,0 @@
-import LocalStorage from './local-storage';
-import Storage from './storage';
-
-export {
-  Storage,
-  LocalStorage,
-};
